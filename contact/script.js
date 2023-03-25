1d4420353ae45939ad223b3d459451cb504ab8df
Yasmin Cibreiros Chagas <yascibre@gmail.com>
2023-03-24 21:57:44 -0300

    add

--- contact/script.js
+++ contact/script.js
@@ -1,33 +1,27 @@
-const nav_icon = document.querySelector('#menu-burguer').addEventListener('click', function () {
-   const nav_menu = document.querySelector('#container-menu')
+const nameInput = document.querySelector('#name')
+const lastName = document.querySelector('#last-name')
+const email = document.querySelector('#email')
+const fieldText = document.querySelector('#fieldMsg')
+
+const msgBox = document.querySelector('.msg-box')
+
+document.querySelector('#btn').addEventListener('click', function () {
+   if (nameInput.value.length == 0 || lastName.value.length == 0 || email.value.length == 0 || fieldText.value.length == 0) {
+    alert('Campo vazio.')
+   } else {
+      msgBox.style.display = 'flex'
+   }
+   document.querySelector('#close').addEventListener('click', function () {
+      msgBox.style.display = 'none'
+   })
+})
+
+const nav_menu = document.querySelector('#container-menu')
+document.querySelector('#menu-burguer').addEventListener('click', function () {
    if(nav_menu.style.display = 'none'){
       nav_menu.style.display = 'block'
    } else {
-      nav_menu.style.display = 'none'
+      nav_menu.style.display = 'none' //nao funciona
    }
-
 })
 
-// const nameInput = document.querySelector('#name').value
-// const lastName = document.querySelector('#last-name').value
-// const email = document.querySelector('#email').value
-
-// const msgBox = document.querySelector('.msg-box')
-
-// document.querySelector('#btn').addEventListener('click', function () {
-//    if (nameInput.length === 0 || lastName.length === 0 || email.length === 0) {
-//     alert('campo vazio')
-//    }
-
-// console.log(nameInput, lastName, email)
-
-// })
-
-// if(nameInput == " " && lastName == " " && email == " ") {
-//     alert('Campos vazios.')
-//  } else {
-//     msgBox.style.display = 'flex'
-//  }
-//  document.querySelector('#close').addEventListener('click', function () {
-//     msgBox.style.display = 'none'
-//     })
